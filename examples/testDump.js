48dee153e3f136d6dccfa7059762b261e6d9548d
Joshua Ball <joshua.g.s.ball@gmail.com>
2014-12-09 06:14:05 -0800

    Adding more examples Fixing stack call.

--- examples/testDump.js
+++ examples/testDump.js
@@ -3,3 +3,18 @@
 var contraLog = require('../lib/')(module);
 
 contraLog.dump('contraLog', contraLog);
+
+var foo = function(){
+    contraLog.dumpWithStack('contraLog', contraLog);
+};
+
+function bar(){
+    contraLog.dumpWithStack('contraLog', contraLog);
+};
+
+var baz = function(){
+    foo();
+    bar();
+};
+
+baz();
