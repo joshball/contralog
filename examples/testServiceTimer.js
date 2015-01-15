0c9e054477cfb487df779748b2917ac6899b43dd
Joshua Ball <joshua.g.s.ball@gmail.com>
2015-01-14 16:42:39 -0800

    Fixing header/stack/service dumps to dump on trace. Passing in color too

--- examples/testServiceTimer.js
+++ examples/testServiceTimer.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var contraLog = require('../lib/')(module);
+var contraLog = require('../lib/')(module).on();
 
 var sc = contraLog.serviceCallStart('github');
 setTimeout(function(){
-    contraLog.serviceCallEnd(sc, sc);
+    contraLog.serviceCallEnd(sc, { foo: 'bar', x: 1});
 }, 2000);
 
